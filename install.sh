0336fb21a6844698ee5aed857450d081f88794d6
Rafael Araujo <araujrafa@gmail.com>
2018-07-24 23:24:10 -0300

    Update install.sh
    Ajustando instalação do code

--- install.sh
+++ install.sh
@@ -95,11 +95,11 @@ fi
 installed "OH my zsh"
 
 if has_not code; then
-  sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-  mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  sudo curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+  sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
   sudo apt update
-  suso apt install code
+  sudo apt install code
 if
 
 # Clean up
